43bd6887a8ca834f64d2ef93aa992c01b945814e
Temur <gapparovte@gmail.com>
2023-08-09 13:28:44 +0500

    projec5

--- Example005_Yes/Program.cs
+++ Example005_Yes/Program.cs
@@ -1,11 +1,16 @@
-﻿Console.Write("Введите имя пользователя: ");
+﻿// Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if (username == "Маша")
+
+// Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+if (username.ToLower() == "Маша")
 {
     Console.WriteLine("Ура, это же МАША !");
 }
 else
 {
-    Console.Write("Привет, " + username);
+    Console.Write("Привет, " + username +"!");
 }
