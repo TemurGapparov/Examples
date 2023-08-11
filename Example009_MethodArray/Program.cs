9cfe5f913d199a9da891bd4d5e0d03231453e7ed
Temur <gapparovte@gmail.com>
2023-08-11 23:07:53 +0500

    Projec9_1

--- Example009_MethodArray/Program.cs
+++ Example009_MethodArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 3, 56, 8, 9, 20, 8, 6 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
